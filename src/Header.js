0abb68d32858d166af1247f783d0e270462e2fcf
JJ <168820998+Jayawardhana22@users.noreply.github.com>
2024-10-17 22:50:56 +0530

    add search bar

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from 'react';
+import "./Header.css";
+import SearchIcon from '@mui/icons-material/Search';
+
+
 
 function Header() {
   return  <nav className="header">
-      <img className='header__logo' src="/amzon-logo.png"
+      <img className='header__logo' src="/amazon-logo.png"
       alt='Amazon logo'
       />
+      
+      <div className='header__search'>
+      <input type='text' className='header__searchInput'/>
+      <SearchIcon className="header__searchIcon"/>
+      </div>
     </nav>;
   
 }
