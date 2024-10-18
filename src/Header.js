4c922ae8fd98e22ff99d414723cef7e87e6c2677
JJ <168820998+Jayawardhana22@users.noreply.github.com>
2024-10-18 23:56:50 +0530

    modify header

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import "./Header.css";
 import SearchIcon from '@mui/icons-material/Search';
+import { Link } from 'react-router-dom';
+import { ShoppingBasket } from '@mui/icons-material';
+import { Zoom } from '@mui/material';
 
 
 
@@ -9,10 +12,56 @@ function Header() {
       <img className='header__logo' src="/amazon-logo.png"
       alt='Amazon logo'
       />
-      
+      <Link to="/">
+      {/*search box */}
       <div className='header__search'>
       <input type='text' className='header__searchInput'/>
       <SearchIcon className="header__searchIcon"/>
+      </div>
+      </Link>
+
+      {/* 3 Links*/}
+      <div className='header__nav'>
+             {/* 1 Link*/}
+             <Link to={"/login"} className='header__link'>
+             <div className='header__option'>
+             <span className='header__optionLineOne'>Hello Qazi</span>
+             <span className='header__optionLineTwo'>Sign In</span>
+             </div>
+
+             </Link>
+
+                  {/* 2 Link*/}
+                  <Link to={"/"} className='header__link'>
+             <div className='header__option'>
+             <span className='header__optionLineOne'>Return</span>
+             <span className='header__optionLineTwo'>& orers</span>
+             </div>
+
+             </Link>
+
+                       {/* 3 Link*/}
+                       <Link to={"/"} className='header__link'>
+             <div className='header__option'>
+             <span className='header__optionLineOne'>Your</span>
+             <span className='header__optionLineTwo'>Prime</span>
+             </div>
+
+             </Link>
+
+             {/* 4 link*/}
+             <Link to={"/checkout"} className='header__link'>
+            <div className='header__optionBasket'>
+              {/* shopping basket Icon */}
+              <ShoppingBasketIco/>
+              {/* Number of items in the basket*/ }
+              <span>0</span>
+              </div>
+              </Link>
+             
+            
+
+
       </div>
     </nav>;
   
