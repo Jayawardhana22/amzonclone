71d844f064a2a5c02f56e7496cbdb967b3ea9181
JJ <168820998+Jayawardhana22@users.noreply.github.com>
2024-10-30 23:53:45 +0530

    add product

--- src/Home.js
+++ src/Home.js
@@ -11,6 +11,7 @@ function Home() {
         alt=''
       />
       {/* Product id, title, price, rating, image */}
+     < div className='home_row'>
       <Product 
         id="12321341"
         title="Sample Product" 
@@ -18,6 +19,50 @@ function Home() {
         rating={5}
         image="/product1.jpg" 
       />
+      <Product 
+        id="12321341"
+        title="Sample Product" 
+        price={11.96}
+        rating={5}
+        image="https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=400" 
+      />
+      
+      </div>
+      <div className='home_row'>
+      <Product 
+        id="12321341"
+        title="Sample Product" 
+        price={11.96}
+        rating={5}
+        image="https://suhadha.lk/wp-content/uploads/2024/02/PRODUCT_169668163467.jpg.webp" 
+      />
+      <Product 
+        id="12321341"
+        title="Sample Product" 
+        price={11.96}
+        rating={5}
+        image="https://www.jbl.com/dw/image/v2/BFND_PRD/on/demandware.static/-/Sites-masterCatalog_Harman/default/dw58d8f63b/1_JBL_PARTYBOX_710_HERO_0031_x8.png?sw=535&sh=535" 
+      />
+      <Product 
+        id="12321341"
+        title="Sample Product" 
+        price={11.96}
+        rating={5}
+        image="https://m.media-amazon.com/images/I/517D9rSAztL.jpg" 
+      />
+
+      </div>
+      <div className='home_row'>
+      <Product 
+        id="12321341"
+        title="Sample Product" 
+        price={11.96}
+        rating={5}
+        image="https://m.media-amazon.com/images/I/61HQByMUOJL.jpg" 
+      />
+
+         
+      </div>
     </div>
   );
 }
