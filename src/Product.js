a92e98f03c48b63975c52369012dde37a47b7ad2
JJ <168820998+Jayawardhana22@users.noreply.github.com>
2024-11-11 23:01:28 +0530

    add check out

--- src/Product.js
+++ src/Product.js
@@ -6,7 +6,7 @@ import { useStateValue } from './StateProvider';
 
 
 function Product({ id, title, image, price, rating }) {
-  const[{basket},dispatch] = useStateValue();
+  const[{},dispatch] = useStateValue();
 
   const addToBasket = () => {
     //add item to basket...
