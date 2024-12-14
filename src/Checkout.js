3ffd87a4238616995fe548c39ef063c583bc53fb
JJ <168820998+Jayawardhana22@users.noreply.github.com>
2024-12-15 00:01:03 +0530

    add checkout css and add subtotal js and css

--- src/Checkout.js
+++ src/Checkout.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { useStateValue } from './StateProvider';
 import "./Checkout.css";
 import CheckoutProduct from './CheckoutProduct';
+import Subtotal from './Subtotal';
 
 function Checkout() {
   const [{ basket }] = useStateValue();
 
   return (
     <div className="checkout">
+      <div className='checkout__left'>
       <img
         className="checkout_ad"
         src="https://images-na.ssl-images-amazon.com/images/G/01/amazongo/kondo/test/Mobile_750x300_01.jpg"
@@ -25,7 +27,7 @@ function Checkout() {
           <h2 className='checkout__title'>Your shopping basket</h2>
           {/* List out all of the checkout products*/}
           {basket?.map((item) =>{
-           
+           return (
          <CheckoutProduct
             id ={item.id}
             title ={item.title}
@@ -33,11 +35,18 @@ function Checkout() {
             price={item.price}
             rating={item.rating}
             />
-      
+           );
 
           })}
         </div>
       )}
+    </div>
+    {basket.length > 0 &&(
+      <div className='chekout__right'>
+        <h1>Subtotal</h1>
+      <Subtotal />
+        </div>
+    )}
     </div>
   );
 }
